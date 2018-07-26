92df0730b798725fe632079542f396c2375d2456
Diogo Paza <diogopazacvel@gmail.com>
2018-07-26 18:35:59 -0300

    final

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var fs = require('fs')
 
+  /*
 try{
 
+  
     fs.unlinkSync('1.txt')
     console.log('Arquivo ecluido com sucesso')
 }catch(err){
     console.log('Erro ao deletar file')
-}
+   
+
+    }
+
+
+    fs.unlink('1.txt', function(err){
+        console.log('excluido com sucesso')
+    })
+ */
+
+ fs.rename('teste/2.txt', '3.txt', function(err){
+     if (err) console.log('Deu erro')
+        
+ })
